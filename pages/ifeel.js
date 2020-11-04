213d30d0265bb14cb8e95303e019a2b8ba5ff690
Idoia <idoia@ifeelonline.com>
2020-11-04 11:45:41 +0100

    add all ifeel page

---
+++ pages/ifeel.js
@@ -0,0 +1,10 @@
+const Ifeel = () => {
+  const url_staging = 'https://staging.ifeelonline.com/';
+  // const url_local = 'http://localhost:3000/widgets/users/login';
+  const url_local = 'http://localhost:3000/';
+  return(
+    <iframe src={url_staging} width="900" height="600" frameBorder="1"></iframe>
+  )
+}
+
+export default Ifeel;
--- pages/widget.js
+++ pages/widget.js
@@ -1,8 +1,9 @@
 const Widget = () => {
   const url_staging = 'https://staging.ifeelonline.com/widgets/users/login';
-  const url_local = 'http://localhost:3000/widgets/users/login';
+  // const url_local = 'http://localhost:3000/widgets/users/login';
+  const url_local = 'http://localhost:3000/widgets/conversations/patient';
   return(
-    <iframe src={url_staging} width="900" height="600" frameBorder="0"></iframe>
+    <iframe src={url_staging} width="900" height="600" frameBorder="1"></iframe>
   )
 }
 
