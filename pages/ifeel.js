bd4ff65eac9b7e03e77bc3c4a46eb9bec0effa88
Idoia <idoia@ifeelonline.com>
2020-11-10 13:33:20 +0000

    try

--- pages/ifeel.js
+++ pages/ifeel.js
@@ -1,7 +1,23 @@
+import {useEffect} from 'react';
+
 const Ifeel = () => {
   const url_staging = 'https://staging.ifeelonline.com/';
   // const url_local = 'http://localhost:3000/widgets/users/login';
   const url_local = 'http://localhost:3000/';
+  useEffect(() => {
+    window.addEventListener('load', () => {
+      console.log('LISTO!');
+      navigator.getUserMedia = ( navigator.getUserMedia ||
+        navigator.webkitGetUserMedia ||
+        navigator.mozGetUserMedia ||
+        navigator.msGetUserMedia);
+      navigator.getUserMedia({audio:true,video:true}, function(stream) {
+        stream.getTracks().forEach(x=>x.stop());
+      }, err=>console.log(err));
+      
+    });
+    
+  }, [])
   return(
     <iframe src={url_staging} width="900" height="600" frameBorder="1"></iframe>
   )
--- pages/widget.js
+++ pages/widget.js
@@ -1,9 +1,29 @@
+import {useEffect} from 'react';
+
 const Widget = () => {
   const url_staging = 'https://staging.ifeelonline.com/widgets/users/login';
-  // const url_local = 'http://localhost:3000/widgets/users/login';
-  const url_local = 'http://localhost:3000/widgets/conversations/patient';
+  const url_local = 'http://localhost:3000/widgets/users/login';
+  // const url_local = 'http://localhost:3000/widgets/conversations/patient';
+  // const url_local = 'http://localhost:3000/';
+  useEffect(() => {
+    window.addEventListener('load', () => {
+      console.log('LISTO!');
+      navigator.getUserMedia = ( navigator.getUserMedia ||
+        navigator.webkitGetUserMedia ||
+        navigator.mozGetUserMedia ||
+        navigator.msGetUserMedia);
+      navigator.getUserMedia({audio:true,video:true}, function(stream) {
+        stream.getTracks().forEach(x=>x.stop());
+      }, err=>console.log(err));
+      
+    });
+    
+  }, [])
+    
   return(
-    <iframe src={url_staging} width="900" height="600" frameBorder="1"></iframe>
+    <>
+      <iframe src={url_staging} width="900" height="600" frameBorder="1"></iframe>
+    </>
   )
 }
 
